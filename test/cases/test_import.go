40d5615d8ade6621170fb5511417b98afd5cc3a6
SongDaian <18921159828@163.com>
2024-05-16 10:42:28 +0800

    add import cases and test

---
+++ test/cases/test_import.go
@@ -0,0 +1,17 @@
+func (a int) Add(b int, c int) int {
+    return a + b + c
+}
+
+import "fmt1"
+import myfmt "fmt2"
+import (
+    "fmt3"
+    "math"
+)
+import (
+    "fmt4"
+    m "math"
+)
+import . "fmt5"
+import _ "github.com/example/package"
+
---
+++ test/lang_parser/test_import.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+
+command = os.path.realpath("/app/experiment_2/src/lian/lang/main.py") +\
+     " --lang=go -debug -print_statements " +\
+         os.path.realpath("/app/experiment_2/test/cases/test_import.go")
+
+print(command)
+
+os.system(command)
